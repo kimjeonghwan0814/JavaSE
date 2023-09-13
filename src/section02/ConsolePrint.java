9ae3f44f92f57745dee5b8fdacdca6ffdc619eac
kimjeonghwan0814 <everydayeffort@naver.com>
2023-09-13 17:16:13 +0900

    consoleprint

---
+++ src/section02/ConsolePrint.java
@@ -0,0 +1,40 @@
+package section02;
+/*
+ * 자바 클래스명 규칙
+ *  1.클래스명은 통상적으로 대문자로 시작한다
+ *  2. 유효한 식별자(예 : 문자, 숫자, 밑줄의 조합)으로 이루어져야 한다
+ *  3. 공백 포함해서는 안 된다
+ *  4. 예약어를 사용해서는 안 된다
+ * 
+ * 낙타표기법 consoleprint
+ * 스네이크표기법 console_print
+ * 
+ * 출력문
+ *  데이터를 화면에 출력하는 방법
+ *  System.out.println():괄호 안 내용 출력 후 행 바꿈
+ *  System.out.print():괄호 안 내용 출력 후 행 바꿈 X
+ *  System.out.printf():문자열 서식 문자를 이용해 형식화된 내용 출력 후 행 바꿈 X
+ *  
+ *  서식문자
+ *   %d : 정수(10진수)
+ *   %o : 정수(8진수)
+ *   %x : 정수(16진수)
+ *   %f : 실수
+ *   %s : 문자열
+ *   %c : 문자
+ */
+
+public class ConsolePrint {
+	//자바 프로그램 시작 메인 메서드
+	public static void main(String[] args) {
+		//print - 줄바꿈 X
+		System.out.print("Welcome.");
+		//pritnln - 줄바꿈 O
+		System.out.println("Java, World");
+		
+		System.out.printf("오늘은 %d월 %d일 입니다.\n", 9, 12);
+		System.out.println("오늘은 " + 9 + "월 " + 12 + "일 입니다.");
+		
+		System.out.printf("%d은 첫번쨰, %f는 두번째, %s는 세번째.\n", 1, 2.0, "셋");
+	}
+}

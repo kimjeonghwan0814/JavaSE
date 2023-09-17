c26990d34b93a5ae34526b470d2a6b2de0fb9fab
kimjeonghwan0814 <everydayeffort@naver.com>
2023-09-17 14:04:41 +0900

    ddd

---
+++ src/section05/Loop04.java
@@ -0,0 +1,12 @@
+package section05;
+
+public class Loop04 {
+	public static void main(String[] args) {
+		for(int i = 0; i<7; i++) {
+			for(int j = 0; j<7; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}

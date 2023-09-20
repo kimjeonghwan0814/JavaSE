37bccdf1a397fef4652c056147b071a428f0367f
kimjeonghwan0814 <everydayeffort@naver.com>
2023-09-20 16:51:43 +0900

    dd

--- src/section05/Loop01.java
+++ src/section05/Loop01.java
@@ -11,6 +11,9 @@ package section05;
  * break 문
  * 	반복문에서 break명령어는 해당 반복문 즉시 종료하는 명령어
  *
+ * continue 문
+ * 	반복문에서 해당 반복을 건너뛰고 다음 반복을 실행하도록 강제하는 명령어
+ *
  */
 
 public class Loop01 {
--- src/section05/Loop02.java
+++ src/section05/Loop02.java
@@ -1,5 +1,18 @@
 package section05;
+/*
+ * 
+ * 2. do ~ while 문
+ * 	while문과 비슷하지만, 최초 한 번 이상 실행되는 반복문
+ * 
+ * 
+ */
 
 public class Loop02 {
-
+	public static void main(String[] args) {
+		int count = 0;
+		do {
+			System.out.println(count);
+			count++;
+		}while(count < 100);
+	}
 }
